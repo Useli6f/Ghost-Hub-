47044bdc000e2afca66f3d6f2cf61463ff8a6212
Useli6f <157888921+Useli6f@users.noreply.github.com>
2024-08-08 20:58:13 +0700

    Panda.bounty .lua

--- Panda.bounty .lua
+++ Panda.bounty .lua
@@ -1,6 +1,3 @@
-
-print("discord.gg/kaymobile1")
- ---------------------------------------------
 repeat task.wait() until game:IsLoaded()
 repeat task.wait() until game.Players
 repeat task.wait() until game.Players.LocalPlayer
